410cb1ffd3e03a4baee1cdd7b7c617065cefea49
vinayShende <vinayjshende@gmail.com>
2023-07-15 11:24:59 +0530

    assignment done

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,11 +3,12 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
+        super(name,4,4,6,isManual,"F1",1);
         //Use arbitrary values for parameters which are not mentioned
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed =getCurrentSpeed()+rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -20,6 +21,20 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            super.stop();
+            super.changeGear(1);
+        } else if (newSpeed>=1 && newSpeed<=50 ) {
+            super.changeGear(1);
+        } else if (newSpeed>=51 && newSpeed<=100) {
+            changeGear(2);
+        } else if (newSpeed>=101 && newSpeed<=150) {
+            super.changeGear(3);
+        } else if (newSpeed>=151 && newSpeed<=200){
+            super.changeGear(4);
+        } else if (newSpeed>=201 && newSpeed<=250){
+            super.changeGear(5);
+        } else if(newSpeed>250){
+            super.changeGear(6);
         }
         //for all other cases, change the gear accordingly
 
--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -1,27 +1,38 @@
 package com.driver;
 
 public class Vehicle {
-
     private String name;
     private int currentSpeed;
     private int currentDirection;
 
     public Vehicle(String name) {
-
+         this.name= this.name;
+         this.currentSpeed=0;
+         this.currentDirection=0;
+    }
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
+    public int getCurrentDirection() {
+        return currentDirection;
     }
 
     public void steer(int direction){
+        //currentSpeed=0;
+        this.currentDirection+=direction;
         //direction is in degrees, add it to the current direction
         System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
     }
 
     public void move(int speed, int direction){
+        this.currentSpeed=speed;
+        this.currentDirection=direction;
         //set the values of currentSpeed and currentDirection
         System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
     }
 
     public void stop(){
-
+        this.currentSpeed=0;
         System.out.println("stop method called - The vehicle is stopped");
     }
 
