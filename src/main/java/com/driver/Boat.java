f41c61ea50784657cae4b9d8314617c0a1a2de1d
vinayShende <vinayjshende@gmail.com>
2023-07-15 11:32:44 +0530

    make changes in code 1st commit

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -9,8 +9,23 @@ public class Boat implements WaterVehicle{
         this.capacity = capacity;
     }
 
-    public String getVehicleName() {
+    public String getName(){
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getVehicleName() {
         return this.name;
     }
 
--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -10,11 +10,28 @@ public class Vehicle {
          this.currentSpeed=0;
          this.currentDirection=0;
     }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getCurrentSpeed() {
         return currentSpeed;
     }
+
+    public void setCurrentSpeed(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
     public int getCurrentDirection() {
         return currentDirection;
+    }
+
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
     }
 
     public void steer(int direction){
