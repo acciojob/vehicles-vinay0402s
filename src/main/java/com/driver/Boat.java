cf3fbf069618ab8a3b2031cc97b9fd14e380dc9e
vinayShende <vinayjshende@gmail.com>
2023-08-03 21:26:04 +0530

    make some changes

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -8,22 +8,6 @@ public class Boat implements WaterVehicle{
         this.name = name;
         this.capacity = capacity;
     }
-
-  /*  public String getName(){
-        return name;
-    }*/
-
-    /*public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }*/
     @Override
     public String getVehicleName() {
         return this.name;
